7ec94ef12cfd33ad980e1a8617cbd543760d2f6c
Berktuğ Denk <126001719+berkdenk@users.noreply.github.com>
2024-09-22 15:34:21 +0200

    Update

---
+++ Level01/ft_putstr/ft_putstr.c
@@ -0,0 +1,21 @@
+/* Write a function that displays a string on the standard output.
+
+The pointer passed to the function contains the address of the string's first
+character.
+
+Your function must be declared as follows:
+
+void	ft_putstr(char *str);*/
+
+void	ft_putstr(char *str)
+{
+    int i;
+
+    i = 0;
+    while(str[i])
+    {
+        write(1, str[i], 1);
+        i++;
+    }
+    write(1, "\n", 1);
+}
